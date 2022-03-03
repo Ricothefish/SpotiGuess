f5b201dcfd90591d0047c8985e1653593fb37923
ricothefish <aymeric.granier@gmail.com>
2022-03-03 20:56:44 +0100

    first commit

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ import './styles/login.css'
 
 
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=80256b057e324c5f952f3577ff843c29&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=80256b057e324c5f952f3577ff843c29&response_type=code&redirect_uri=https://blindtest-spotify-v1.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 
 
