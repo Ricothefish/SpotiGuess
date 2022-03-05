fbb26863e0a2e9aa623d78b34b07bae0781e78cd
ricothefish <aymeric.granier@gmail.com>
2022-03-05 12:52:31 +0100

    cht urlLogin

--- src/Login.js
+++ src/Login.js
@@ -3,11 +3,13 @@ import React from 'react'
 import './styles/login.css'
 
 
-const urlLocal = 'http://localhost:3000'
-const urlOnline= 'https://blindtest-spotify.netlify.app'
+const urlClientLocal = 'http://localhost:3000'
+const urlClientOnline= 'https://blindtest-spotify.netlify.app'
+
+const urlClient = urlClientOnline
 
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=80256b057e324c5f952f3577ff843c29&response_type=code&redirect_uri=${urlOnline}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=80256b057e324c5f952f3577ff843c29&response_type=code&redirect_uri=${urlClient}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 console.log('url',AUTH_URL)
 
--- src/UseAuth.js
+++ src/UseAuth.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"
 
 
+const urlServerLocal = 'http://localhost:3001'
+const urlServerOnline= 'https://blindtest-spotify-v1.herokuapp.com'
 
+const urlServer= urlServerOnline;
 
 function UseAuth(code) {
     const [accessToken, setAccessToken] = useState()
@@ -11,7 +14,7 @@ function UseAuth(code) {
 
 
     useEffect(() => {
-        axios.post('https://blindtest-spotify-v1.herokuapp.com/login', { code, })
+        axios.post(`${urlServer}/login`, { code, })
             .then(res => {
                 setAccessToken(res.data.accessToken)
                 setRefreshToken(res.data.refreshToken)
@@ -30,7 +33,7 @@ function UseAuth(code) {
         if (!refreshToken || !expiresIn) return
 
         const interval = setInterval(() => {
-            axios.post('https://blindtest-spotify-v1.herokuapp.com/refresh', { refreshToken, })
+            axios.post(`${urlServer}/refresh`, { refreshToken, })
                 .then(res => {
                     setAccessToken(res.data.accessToken)
                     setExpiresIn(res.data.expiresIn)
