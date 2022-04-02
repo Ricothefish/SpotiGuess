0d45fc15b6158c042173b78c45b9e2c84336fce0
ricothefish <aymeric.granier@gmail.com>
2022-04-02 22:28:06 +0200

    explicit algo

--- src/UseAuth.js
+++ src/UseAuth.js
@@ -5,7 +5,7 @@ import axios from "axios"
 const urlServerLocal = 'http://localhost:3001'
 const urlServerOnline= 'https://blindtest-spotify-v1.herokuapp.com'
 
-const urlServer= urlServerLocal;
+const urlServer= urlServerOnline;
 
 function UseAuth(code) {
     const [accessToken, setAccessToken] = useState()
